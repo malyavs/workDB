9d0ab0749ed9bafc72e863917b9cce41a69ac4a1
malyavs <malyavamalyavko@yandex.by>
2026-06-06 17:38:19 +0300

    fix BD

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import datasets
-from datasets import load_dataset
-dataset = load_dataset("imdb")
-print("daf")
-print(dataset)
+import pandas as pd
+
+# Прямая ссылка на CSV-файл на GitHub
+url = "https://raw.githubusercontent.com/mwaskom/seaborn-data/master/tips.csv"
+
+# Загрузка занимает одну строчку
+df = pd.read_csv(url)
+
+print(df) # Показывает первые 5 строк таблицы
