63f50325fabdb27141941c08851dbc7356a0593f
malyavs <malyavamalyavko@yandex.by>
2026-06-07 23:18:11 +0300

    add new copy func

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ import pandas as pd
 
 # Вместо загрузки из URL мы читаем локальный файл
 local_df = pd.read_csv('tips_data.csv')
-
-# Теперь можно делать любые операции
-print(local_df)
+def copy_file(new_file, old_file):
+    with open(old_file, 'r') as f:
+        with open(new_file, 'w') as f2:
+            for line in f:
+                f2.write(line)
+if __name__ == '__main__':
+    copy_file('data.txt', 'tips_data.csv')
