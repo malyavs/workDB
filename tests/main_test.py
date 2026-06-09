b996c8a99730d360dbced12095ee51e32ddcd04e
malyavs <malyavamalyavko@yandex.by>
2026-06-09 20:59:30 +0300

    update tests

--- tests/main_test.py
+++ tests/main_test.py
@@ -2,4 +2,9 @@ from master import main
 
 
 def test_clean(file):
-    assert main.clean_file(file) == None
+    with open(file, 'w',encoding="UTF-8") as f:
+        f.write("test")
+    with open(file,'r',encoding="UTF-8") as f:
+        content = f.read()
+    assert content == "test"
+    assert main.clean_file(file) is None
