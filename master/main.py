daadad1d60c7d2742644ae9ddbd231f7956da398
malyavs <malyavamalyavko@yandex.by>
2026-06-09 21:00:40 +0300

    add execution try

--- master/main.py
+++ master/main.py
@@ -2,14 +2,21 @@ import pandas as pd
 
 # Вместо загрузки из URL мы читаем локальный файл
 def copy_file(new_file, old_file):
-    with open(old_file, 'r') as f:
-        with open(new_file, 'w') as f2:
-            for line in f:
-                f2.write(line)
+    try:
+        with open(old_file, 'r',encoding="UTF-8") as f:
+            with open(new_file, 'w') as f2:
+                for line in f:
+                    f2.write(line)
+    except (FileNotFoundError, FileExistsError) as e1:
+        raise ValueError(f"I don't have file {e1}")
 def clean_file(file):
-    with open(file, 'w') as f:
-        with open(file, 'r') as f2:
-            for line in f2:
-                line=""
+    try:
+        with open(file, 'w',encoding="UTF-8") as f:
+            f.write('')
+    except (FileNotFoundError, FileExistsError) as e:
+        raise ValueError(f"I don't have file {e}")
+def main():
+    clean_file('info/data.txt')
+    copy_file("info/data.txt","info/tips_data.csv")
 if __name__ == '__main__':
-    copy_file('info/data.txt', 'info/tips_data.csv')
+    main()
