7ebfdcb71e1dc16a7d09937c3460c613c95bc9d6
malyavs <malyavamalyavko@yandex.by>
2026-06-14 21:17:04 +0300

    add docstrings for pylint

--- master/main.py
+++ master/main.py
@@ -1,7 +1,13 @@
-import pandas as pd
-
-# Вместо загрузки из URL мы читаем локальный файл
+"""
+    file for 
+"""
 def copy_file(new_file, old_file):
+    """
+    func for copying file
+    :param new_file:
+    :param old_file:
+    :return:
+    """
     try:
         with open(old_file, 'r',encoding="UTF-8") as f:
             with open(new_file, 'w') as f2:
@@ -10,12 +16,21 @@ def copy_file(new_file, old_file):
     except (FileNotFoundError, FileExistsError) as e1:
         raise ValueError(f"I don't have file {e1}")
 def clean_file(file):
+    """
+    cleaning file
+    :param file:
+    :return:
+    """
     try:
         with open(file, 'w',encoding="UTF-8") as f:
             f.write('')
     except (FileNotFoundError, FileExistsError) as e:
         raise ValueError(f"I don't have file {e}")
 def main():
+    """
+    main function
+    :return:
+    """
     clean_file('info/data.txt')
     copy_file("info/data.txt","info/tips_data.csv")
 if __name__ == '__main__':
