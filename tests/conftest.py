04dd3f2e2c8fad2060b819d3f744f6dc45556402
malyavs <malyavamalyavko@yandex.by>
2026-06-16 12:09:56 +0300

    fixed tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,19 @@
+from fileinput import close
+from os import remove
 import pytest
 from master import main
 
-
 # Декоратор @pytest.fixture сообщает Pytest, что это фикстура
 @pytest.fixture
-def file():
-    # Этот код выполнится перед каждым тестом, который запросит эту фикстуру
-    print("Подготовка файла...")
-    main.copy_file('info/data.txt', 'info/tips_data.csv')
-    return 'info/data.txt' # Возвращаем имя файла, которое попадет в аргумент теста
+def two_temp_files():
+    with open('info/tips_data.csv', 'r') as f:
+        content = f.read()
+    file="first_file_with_text.txt"
+    second_file="second_file_without_text.txt"
+    with open(file, "w") as f:
+        f.write(content)
+    with open(second_file, 'w') as f:
+        f.write("")
+    yield file, second_file
+    remove(file)
+    remove(second_file)
--- tests/main_test.py
+++ tests/main_test.py
@@ -1,10 +1,30 @@
 from master import main
 
 
-def test_clean(file):
-    with open(file, 'w',encoding="UTF-8") as f:
-        f.write("test")
-    with open(file,'r',encoding="UTF-8") as f:
+
+def test_clean(two_temp_files):
+    src_file, dst_file = two_temp_files
+    with open(src_file,'r',encoding="UTF-8") as f1:
+        old_content1 = f1.read()
+    with open(dst_file,'w',encoding="UTF-8") as f:
+        f.write("ZEBRA")
+    with open(dst_file,'r',encoding="UTF-8") as f2:
+        old_content2 = f2.read()
+    assert old_content2 == "ZEBRA"
+    assert main.clean_file(dst_file) == None
+    main.copy_file(dst_file,src_file)
+    with open(dst_file,'r',encoding="UTF-8") as f1:
+        assert old_content1 == f1.read()
+        main.clean_file(dst_file)
+
+"""def test_copy(new_file, old_file):
+    with open(new_file,'r',encoding="UTF-8") as f:
         content = f.read()
-    assert content == "test"
-    assert main.clean_file(file) is None
+    with open(old_file,'r',encoding="UTF-8") as f:
+        content2 = f.read()
+    if content != content2:
+        main.copy_file("data.txt","tips_data.txt")
+    with open(new_file,'r',encoding="UTF-8") as f:
+        new_content = f.read()
+        assert new_content == content2
+"""
